dfad63d470d9b47f80f5f44b3f4a0f0f8d7fc9c5
ArenDeveloper05 <antisystem005@mail.ru>
2023-07-18 21:14:54 +0400

    delete comment

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,20 +5,6 @@ const Header = ({ config, imSiracTivy }) => {
   return (
     <header>
       <Nav config={config} imSiracTivy={imSiracTivy} />
-      {/* <nav>
-        <div className="nav-item">
-          <a href="#Home">
-            {config.navConfig.home}
-            {imSiracTivy}
-          </a>
-        </div>
-        <div className="nav-item">
-          <a href="#About">{config.navConfig.about}</a>
-        </div>
-        <div className="nav-item">
-          <a href="#Contact">{config.navConfig.contact}</a>
-        </div>
-      </nav> */}
     </header>
   );
 };
